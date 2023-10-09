eae4b50666b70769158b5894b4c23e68be596e0f
OST <cpython.projects@gmail.com>
2023-10-09 10:27:14 +0200

    fuzzy controller

---
+++ main.py
@@ -0,0 +1,22 @@
+import model
+import inference_mamdani
+import fuzzy_operators
+
+crisp = [22, 185, 20, 50_000]
+inference_mamdani.preprocessing(model.input_lvs, model.output_lv)
+result = inference_mamdani.process(model.input_lvs, model.output_lv, model.rule_base, crisp)
+
+print(result)
+
+for lv in model.input_lvs:
+    fuzzy_operators.draw_lv(lv)
+fuzzy_operators.draw_lv(model.output_lv)
+
+
+
+
+
+
+
+
+
