1aa05c1668f61b4fcc11cee3f98ad8d826f0adb9
OST <oleh.s.tymchuk@gmail.com>
2024-10-14 13:41:53 +0200

    fuzzy controller

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import model
 import inference_mamdani
 import fuzzy_operators
 
-crisp = [22, 185, 20, 50_000]
+crisp = [8, 3.5, 65]
+
 inference_mamdani.preprocessing(model.input_lvs, model.output_lv)
 result = inference_mamdani.process(model.input_lvs, model.output_lv, model.rule_base, crisp)
 
@@ -11,8 +12,6 @@ print(result)
 for lv in model.input_lvs:
     fuzzy_operators.draw_lv(lv)
 fuzzy_operators.draw_lv(model.output_lv)
-
-
 
 
 
